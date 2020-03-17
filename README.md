03c843eec024eac3f044f13f3e2d3b098c743017
Jack <jack.wiley10@gmail.com>
2020-03-16 20:12:04 -0400

    fjdslfk

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a portfoli of all my unit projects using node and express
+type npm start to run and go to local host 3000
---
+++ app.js
@@ -0,0 +1,43 @@
+
+const express = require('express');
+
+
+
+const app = express();
+
+
+
+app.use('/static', express.static('public'));
+
+
+app.set('view engine', 'pug');
+
+
+
+const mainRoutes = require('./routes');
+app.use(mainRoutes);
+
+
+
+
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+
+
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.render('error', err);
+});
+
+
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
